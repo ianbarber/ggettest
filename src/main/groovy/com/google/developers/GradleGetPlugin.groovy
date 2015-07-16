01fbd8e1c3c6f0da28617d999c5891b5b359f555
Ian Barber <ianbarber@google.com>
2015-07-15 21:46:15 -0700

    Working git clone

--- src/main/groovy/com/google/developers/GradleGetPlugin.groovy
+++ src/main/groovy/com/google/developers/GradleGetPlugin.groovy
@@ -1,13 +1,31 @@
 package com.google.developers
 
+import org.ajoberstar.grgit.Grgit
 import org.gradle.api.Project
 import org.gradle.api.Plugin
+import java.security.MessageDigest
+
 
 class GradleGetPlugin implements Plugin<Project> {
     void apply(Project target) {
         /*target.task('get', type: GreetingTask)*/
 		target.task('get') << {
-            println "Getting"
+			// TODO: Can we work this out automatically?
+			def mavenRepo = "~/.m2/repository"
+			def githubURL = "https://github.com/ianbarber/ggettest"
+			
+            println "Getting " + githubURL
+		    String urlhash = MessageDigest.getInstance('MD5').digest(githubURL.bytes).encodeHex().toString()
+			
+			File dir=new File('/tmp/' + urlhash)
+			if(dir.exists()) {
+				println '/tmp/' + urlhash + ' exists, exiting.'
+				return
+			}
+			def grgit = Grgit.clone(dir: dir, uri: githubURL)			
+			
+			
+			// TODO: Clean up clone URL directory.
         }
     }
 }
